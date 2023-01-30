a1cdd3ab25621b283d15868db811f4635135598e
Lucas <lucasjiang207@gmail.com>
2023-01-30 16:15:50 +0800

    Update Providers.js

--- Providers.js
+++ Providers.js
@@ -14,3 +14,27 @@ const balanceGoerli = await providerGoerli.getBalance(`vitalik.eth`);
 console.log(`ETH Balance of vitalik: ${ethers.utils.formatEther(balance)} ETH`);
 console.log(`Goerli ETH Balance of vitalik: ${ethers.utils.formatEther(balanceGoerli)} ETH`);
 
+console.log("\n2. Query which chain the provider is connected to")
+const network = await providerETH.getNetwork();
+console.log(network);
+
+console.log("\n3. Query block height")
+const blockNumber = await providerETH.getBlockNumber();
+console.log(blockNumber);
+
+console.log("\n4. Check current gas price")
+const gasPrice = await providerETH.getGasPrice();
+console.log(gasPrice);
+
+console.log("\n5. Query the current recommended gas settings")
+const feeData = await providerETH.getFeeData();
+console.log(feeData);
+
+console.log("\n6. Query Block Information")
+const block = await providerETH.getBlock(0);
+console.log(block);
+
+console.log("\n7. Given the contract address to query the contract bytecode, the example uses the WETH address")
+const code = await providerETH.getCode("0xc778417e063141139fce010982780140aa0cd5ab");
+console.log(code);
+
