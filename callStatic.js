b999291cd208d64e4894ecb2c78ba84a0ba3cbbc
Lucas <lucasjiang207@gmail.com>
2023-02-02 13:36:47 +0800

    Update callStatic

--- callStatic.js
+++ callStatic.js
@@ -13,10 +13,11 @@ const addressDAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
 const contractDAI = new ethers.Contract(addressDAI, abiDAI, provider)
 
 const address = await wallet.getAddress()
+const address2 = "0xed55d1b71b6bfa952ddbc4f24375c91652878560"
 console.log("\n1. Read the DAI balance of the test wallet")
-const balanceDAI = await contractDAI.balanceOf(address)
+const balanceDAI = await contractDAI.balanceOf(address2)
 console.log(`DAI positions: ${ethers.utils.formatEther(balanceDAI)}\n`)
 
-console.log("\n2.  Try to call transfer transfer 1 DAI with callStatic, msg.sender is Vitalik's address")
-const tx = await contractDAI.callStatic.transfer("vitalik.eth", ethers.utils.parseEther("10000"), {from: "vitalik.eth"})
+console.log("\n2.  Try to call transfer transfer 1 DAI with callStatic, msg.sender is address2 address")
+const tx = await contractDAI.callStatic.transfer("vitalik.eth", ethers.utils.parseEther("1"), {from: address2})
 console.log(`Will the deal be a success? : `, tx)
